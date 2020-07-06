d38714f85b5a579b09047b1a9ece00b24758c00b
Egor Mityaev <egor.mityaev@spark.co.nz>
2020-07-06 16:29:07 +1200

    add image component

---
+++ ui.frontend/src/components/Image/Image.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MapTo } from '@adobe/cq-react-editable-components';
+import styled from 'styled-components';
+
+const ImageEditConfig = {
+  emptyLabel: 'Image',
+
+  isEmpty: function (props) {
+    return !props || !props.src || props.src.trim().length < 1;
+  },
+};
+
+const ImageSrc = styled.img`
+  margin: 1rem 0;
+  width: 100%;
+  border: 0;
+`;
+
+const Image = (props) => {
+  if (ImageEditConfig.isEmpty(props)) {
+    return null;
+  }
+
+  return (
+    <div className="Image">
+      <ImageSrc
+        src={props.src}
+        alt={props.alt}
+        title={props.title ? props.title : props.alt}
+      />
+    </div>
+  );
+};
+
+export default MapTo('wknd-spa-react/components/image')(Image, ImageEditConfig);
